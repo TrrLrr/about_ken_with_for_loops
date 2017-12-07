893c8645bee761178e2ac75603380b439bd23e67
Taylor Stemple <taylorstemple@gmail.com>
2017-12-07 14:11:25 -0800

    added functions

--- app.js
+++ app.js
@@ -1,46 +1,56 @@
 'use strict';
 
-var seattle = prompt('Is Ken from Seattle?');
-console.log('is ken from Seattle:', seattle);
+function homeTown() {
+  var seattle = prompt('Is Ken from Seattle?');
+  console.log('is ken from Seattle:', seattle);
 
-if (seattle.toLowerCase() === 'no' || seattle.toLowerCase() === 'n') {
-  alert('You are correct. He is from Montana.');
-} else {
-  alert('He is from Montana.');
+  if (seattle.toLowerCase() === 'no' || seattle.toLowerCase() === 'n') {
+    alert('You are correct. He is from Montana.');
+  } else {
+    alert('He is from Montana.');
+  }
 }
 
-var animals = prompt('Does Ken love animals?');
-console.log('animal lover:', animals);
+function animalLover() {
+  var animals = prompt('Does Ken love animals?');
+  console.log('animal lover:', animals);
 
-if (animals.toLowerCase() === 'yes' || animals.toLowerCase() === 'y') {
-  alert('But of course he does!');
-} else {
-  alert('Animals ROCK!');
+  if (animals.toLowerCase() === 'yes' || animals.toLowerCase() === 'y') {
+    alert('But of course he does!');
+  } else {
+    alert('Animals ROCK!');
+  }
 }
 
-var coffee = prompt('Do you think Ken loves coffee?');
-console.log('coffee:', coffee);
+function coffeeLover(){
+  var coffee = prompt('Do you think Ken loves coffee?');
+  console.log('coffee:', coffee);
 
-if (coffee.toLowerCase() === 'yes' || coffee.toLowerCase() === 'y') {
-  alert('You are correct, and there are so many options here.');
-} else {
-  alert('He loves coffee!');
+  if (coffee.toLowerCase() === 'yes' || coffee.toLowerCase() === 'y') {
+    alert('You are correct, and there are so many options here.');
+  } else {
+    alert('He loves coffee!');
+  }
 }
 
-var travel = prompt('Does Ken enjoy traveling?');
-console.log('travel:', travel);
+function travelLover(){
+  var travel = prompt('Does Ken enjoy traveling?');
+  console.log('travel:', travel);
 
-if (travel.toLowerCase() === 'yes' || travel.toLowerCase() === 'y') {
-  alert('Oh so much!');
-} else {
-  alert('There is so much to see.');
+  if (travel.toLowerCase() === 'yes' || travel.toLowerCase() === 'y') {
+    alert('Oh so much!');
+  } else {
+    alert('There is so much to see.');
+  }
 }
 
-var snow = prompt('Does Ken like snow?');
-console.log('like snow:', snow);
+function snowLover() {
+  var snow = prompt('Does Ken like snow?');
+  console.log('like snow:', snow);
 
-if (snow.toLowerCase() === 'yes' || snow.toLowerCase() === 'y') {
-  alert('Very much so.');
-} else {
-  alert('Oh, but he does!');
+  if (snow.toLowerCase() === 'yes' || snow.toLowerCase() === 'y') {
+    alert('Very much so.');
+  } else {
+    alert('Oh, but he does!');
+  }
 }
